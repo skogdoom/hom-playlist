cb2485b7216a4ceb98a97be1d360bc51d7fe254f
David Andreasson <647640+skogdoom@users.noreply.github.com>
2020-10-12 19:10:39 +0200

    works but requires a lot of cleanup

--- playlist.py
+++ playlist.py
@@ -1,5 +1,12 @@
 import requests
 from bs4 import BeautifulSoup, NavigableString
+import json
+from secrets import spotify_client_id, spotify_client_secret, spotify_user_id
+import base64
+import uuid
+import hashlib
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 
 
 def get_bands():
@@ -34,6 +41,91 @@ def print_bands(bands_by_year):
             print(band)
 
 
+def find_songs(artist):
+    print("finding songs for " + artist + "...")
+    sp = get_spotify_client()
+    results = sp.search(q='artist:' + artist, type='artist')
+
+    items = results['artists']['items']
+    track_ids = []
+
+    if len(items) > 0:
+        # print(items[0]['id'])
+        # print(items[0]['name'])
+        artist_id = 'spotify:artist:' + items[0]['id']
+
+        top_tracks = sp.artist_top_tracks(artist_id)
+
+        for track in top_tracks['tracks'][:3]:
+            track_ids.append(track['id'])
+            # print(track['id'])
+            # print(track['name'])
+
+    return track_ids
+
+
+def create_playlist(client, year):
+    print("create playlists...")
+
+    #sp = get_spotify_client_auth()
+
+    playlist = client.user_playlist_create(
+        spotify_user_id,
+        "HOM {}".format(year),
+        False,
+        False,
+        "Songs from the House of Metal lineup {}".format(year)
+    )
+    print(playlist)
+    return playlist['id']
+
+
+def add_songs_to_playlist(client, playlist_id, songs):
+    #sp = get_spotify_client_auth()
+    client.playlist_add_items(playlist_id, songs)
+
+
+def call_spotify_test():
+    sp = get_spotify_client()
+    track_results = sp.search(q='metallica', type='track', limit=5, offset=0)
+    print(track_results)
+
+
+def get_spotify_client():
+    client_credentials_manager = SpotifyClientCredentials(
+        client_id=spotify_client_id,
+        client_secret=spotify_client_secret
+    )
+    return spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+
+def get_spotify_client_auth():
+    scope = "playlist-modify-private"
+
+    auth_manager = SpotifyOAuth(
+        client_id=spotify_client_id,
+        client_secret=spotify_client_secret,
+        scope=scope,
+        redirect_uri="http://localhost"
+    )
+
+    return spotipy.Spotify(auth_manager=auth_manager)
+
+
 if __name__ == '__main__':
     bands_by_year = get_bands()
-    print_bands(bands_by_year)
+    # print_bands(bands_by_year)
+
+    client = get_spotify_client_auth()
+
+    #create_playlist("2020")
+    #add_songs_to_playlist('6MmOQ2bRz7TsjiEyn7aIfO', find_songs('metallica'))
+
+    for year, bands in bands_by_year.items():
+        # create playlist for year
+        # print(year)
+        playlist_id = create_playlist(client, year)
+        for artist in bands_by_year[year]:
+            songs = find_songs(artist)
+            if len(songs) > 0:
+                add_songs_to_playlist(client, playlist_id, songs)
