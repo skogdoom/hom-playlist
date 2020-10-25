4a139f3aba76d2ef970045b7295d00fe82b59151
David Andreasson <647640+skogdoom@users.noreply.github.com>
2020-10-25 15:59:01 +0100

    cleanup

--- playlist.py
+++ playlist.py
@@ -1,95 +1,64 @@
+from secrets import spotify_client_id, spotify_client_secret, spotify_user_id
 import requests
 from bs4 import BeautifulSoup, NavigableString
-import json
-from secrets import spotify_client_id, spotify_client_secret, spotify_user_id
-import base64
-import uuid
-import hashlib
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 from time import sleep
 
-def get_bands():
-    print("Gotta go get a dictionary by year from https://www.houseofmetal.se/en/history/")
 
+def get_bands():
     source = requests.get('https://www.houseofmetal.se/en/history/').text
     soup = BeautifulSoup(source, 'html.parser')
     elements = soup.select('div.et_pb_text_inner')
-    bandsByYear = {}
+    bands_by_year = {}
     for element in elements:
         year = element.find('strong')
 
-        if (year == None):
+        if year is None:
             continue
 
         bands = element.find('p')
 
-        bandList = []
+        band_list = []
         for b in bands:
-            if (isinstance(b, NavigableString)):
-                bandList.append(b)
+            if isinstance(b, NavigableString):
+                band_list.append(b)
 
-        bandsByYear[year.text] = bandList
+        bands_by_year[year.text] = band_list
 
-    return bandsByYear
-
-
-def print_bands(bands_by_year):
-    for year, bands in bands_by_year.items():
-        print(year)
-        for band in bands:
-            print(band)
+    return bands_by_year
 
 
 def find_songs(client, artist):
-    print("finding songs for " + artist + "...")
-    #sp = get_spotify_client()
     results = client.search(q=artist, type='artist')
 
     items = list(filter(lambda i: i['name'] == artist, results['artists']['items']))
     track_ids = []
 
-    #print(list(map(lambda i: i['name'], items)))
-    #print(list(map(lambda i: i['name'], list(filter(lambda i: i['name'] == artist, items)))))
-
     if len(items) > 0:
-        #print(artist + " = " + items[0]['name'])
         artist_id = 'spotify:artist:' + items[0]['id']
         top_tracks = client.artist_top_tracks(artist_id)
 
         for track in top_tracks['tracks'][:3]:
             track_ids.append(track['id'])
-            # print(track['id'])
-            print(track['name'])
 
     return track_ids
 
 
 def create_playlist(client, year):
-    print("create playlists...")
-
-    #sp = get_spotify_client_auth()
-
     playlist = client.user_playlist_create(
         spotify_user_id,
-        "HOM {}".format(year),
+        "HoM {year}",
         True,
         False,
-        "Songs from the House of Metal (houseofmetal.se) lineup {}".format(year)
+        f"Songs from the House of Metal (houseofmetal.se) lineup {year}"
     )
-    print(playlist)
+
     return playlist['id']
 
 
 def add_songs_to_playlist(client, playlist_id, songs):
-    #sp = get_spotify_client_auth()
     client.playlist_add_items(playlist_id, songs)
-
-
-def call_spotify_test():
-    sp = get_spotify_client()
-    track_results = sp.search(q='metallica', type='track', limit=5, offset=0)
-    print(track_results)
 
 
 def get_spotify_client():
@@ -102,36 +71,31 @@ def get_spotify_client():
 
 def get_spotify_client_auth():
     scope = "playlist-modify-public"
-
     auth_manager = SpotifyOAuth(
         client_id=spotify_client_id,
         client_secret=spotify_client_secret,
         scope=scope,
         redirect_uri="http://localhost"
     )
-
     return spotipy.Spotify(auth_manager=auth_manager)
 
+
 def create_playlists():
+    print("Getting bands by year from https://www.houseofmetal.se/en/history/")
     bands_by_year = get_bands()
-    # print_bands(bands_by_year)
 
     client = get_spotify_client_auth()
 
     for year, bands in bands_by_year.items():
+        print(f"Creating playlist for {year}...")
         playlist_id = create_playlist(client, year)
         for artist in bands_by_year[year]:
+            print(f"Finding songs for {artist}...")
             songs = find_songs(client, artist)
             if len(songs) > 0:
                 add_songs_to_playlist(client, playlist_id, songs)
         sleep(5)  # to make sure we don't hit the rate limit
 
+
 if __name__ == '__main__':
     create_playlists()
-
-    # create_playlist("2020")
-    # add_songs_to_playlist('6MmOQ2bRz7TsjiEyn7aIfO', find_songs('metallica'))
-
-    #client = get_spotify_client()
-    #songs = find_songs(client, 'Grid')
-    #print(songs)
